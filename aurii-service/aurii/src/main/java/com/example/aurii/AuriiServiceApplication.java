2c8043b16f5c710252a84a3817aab38995c5367e
Chris Langsford <chris.langsford@entelect.co.za>
2025-05-13 17:32:19 +0200

    added base project from poc repo. Includes prototype of the coach and LLM Interface Modules

---
+++ aurii-service/aurii/src/main/java/com/example/aurii/AuriiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.aurii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuriiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuriiServiceApplication.class, args);
+	}
+
+}
