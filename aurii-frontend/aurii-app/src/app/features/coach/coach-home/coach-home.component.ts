49196609f77db49ab440cf4370a1fed2e579c004
Chris Langsford <chris.langsford@entelect.co.za>
2025-05-28 11:03:14 +0200

    Minor tweaks - removed unnecessary modules

--- aurii-frontend/aurii-app/src/app/features/coach/coach-home/coach-home.component.ts
+++ aurii-frontend/aurii-app/src/app/features/coach/coach-home/coach-home.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'coach-home',
   templateUrl: './coach-home.component.html',
   styleUrls: ['./coach-home.component.scss'],
+  imports: [
+  ]
 })
 export class CoachHomeComponent implements OnInit {
 
